6e19e6f8b5d9d47d39b2d35f8096ede263332d03
Pilu Vitu <paulo.tspi@gmail.com>
2021-12-08 02:34:24 -0300

    add pep8 files

---
+++ pep8.py
@@ -0,0 +1,27 @@
+"""
+PEP8 - Python Enhancement Proposal
+São propostas de melhorias para linguagem Python
+
+The Zen of Python
+
+import this
+
+A ideia da PEP8 é que possamos escrever codigos Python de forma Pythonica
+"""
+
+"""
+[1] - Utilize Camel Case para nomes de classes;
+"""
+
+
+class Calculadora:
+    pass
+
+
+class CalculadoraCientifica:
+    pass
+
+
+"""
+
+"""
