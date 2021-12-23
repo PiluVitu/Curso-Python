fa0a4a34198985b80771c492dd3e87e9ecff82b1
Paulo Victor Torres Silva <paulo.tspi@gmail.com>
2021-12-23 17:54:38 -0300

    Dir e help completos e recebdno dados iniciado

---
+++ dir_and_help.py
@@ -0,0 +1,12 @@
+"""
+Utilitarios python para auxiliar na programação
+dir -> Aprersenta todos os atributos e funções/métodos disponíveis para determinado tipo de dado ou8 variavel
+
+dir(tipo de dado/variável)
+
+help ->Apresenta a documentação/como utilizar os atributos/propriedades e funções/métodos disponiveis para determinado
+tipo de dado ou variavel.
+
+help(tipo de dado/variavel.propriedade)
+
+"""
